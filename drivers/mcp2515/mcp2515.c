874ca3db0f67ee95dc60985318f2ffb7bc51e7cf
tobhe <t.heider@campus.lmu.de>
2017-07-22 18:16:17 +0200

    drivers/mcp2515: initial setup

---
+++ drivers/mcp2515/mcp2515.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2017 Tobias Heider <heidert@nm.ifi.lmu.de>
+ * Copyright (C) 2017 Johannes Walcher <johannes.walcher@tum.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_mcp2515
+ * @{
+ *
+ * @file
+ * @brief       Implementation of public functions for mcp2515 drivers
+ *
+ * @author      Tobias Heider <heidert@nm.ifi.lmu.de>
+ * @author      Johannes Walcher <johannes.walcher@tum.de>
+ *
+ * @}
+ */
