5b8f5ea5cfe55bcdedad07af6f72f2e097443b65
tobhe <t.heider@campus.lmu.de>
2017-09-28 10:24:18 +0200

    sys/memarray: format changes to riot standard

--- sys/memarray/memarray.c
+++ sys/memarray/memarray.c
@@ -6,34 +6,38 @@
  * directory for more details.
  */
 
+#include <string.h>
 #include "memarray.h"
-#include "string.h"
 
-void *memarray_alloc(memarray_t *mem) {
-  if (mem->first_free == NULL) {
-    return NULL;
-  }
-  void *free = mem->first_free;
-  mem->first_free = *((void **)mem->first_free);
-  return free;
-}
-
-void memarray_init(memarray_t *mem) {
-  for (size_t i = 0; i < mem->count-1; i++) {
-    void *next = ((char *)mem->data) + ((i + 1) * mem->size);
-    memcpy(((char *)mem->data) + (i * mem->size), &next, sizeof(void *));
-  }
-}
-
-uint8_t memarray_free(memarray_t *mem, void *ptr) {
-  char *iter = mem->data;
-  for (size_t i = 0; i < mem->count; i++) {
-    if (iter == ptr) {
-      memcpy(iter, &mem->first_free, sizeof(void *));
-      mem->first_free = iter;
-      return 0;
+void *memarray_alloc(memarray_t *mem)
+{
+    if (mem->first_free == NULL) {
+        return NULL;
     }
-    iter += mem->size;
-  }
-  return 1;
+    void *free = mem->first_free;
+    mem->first_free = *((void **)mem->first_free);
+    return free;
+}
+
+void memarray_init(memarray_t *mem)
+{
+    for (size_t i = 0; i < mem->count - 1; i++) {
+        void *next = ((char *)mem->data) + ((i + 1) * mem->size);
+        memcpy(((char *)mem->data) + (i * mem->size), &next, sizeof(void *));
+    }
+}
+
+uint8_t memarray_free(memarray_t *mem, void *ptr)
+{
+    char *iter = mem->data;
+
+    for (size_t i = 0; i < mem->count; i++) {
+        if (iter == ptr) {
+            memcpy(iter, &mem->first_free, sizeof(void *));
+            mem->first_free = iter;
+            return 0;
+        }
+        iter += mem->size;
+    }
+    return 1;
 }
