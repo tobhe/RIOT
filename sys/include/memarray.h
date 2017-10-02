afad7b38a0a15078e3816cceda87d605769f9ee8
tobhe <t.heider@campus.lmu.de>
2017-10-02 09:49:24 +0200

    sys/memarray: More verbose doxygen documentation

--- sys/include/memarray.h
+++ sys/include/memarray.h
@@ -25,21 +25,44 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Memory pool
+ */
 typedef struct {
     void *first_free; /**< head of the free list */
 } memarray_t;
 
 /**
- * Initialize memory
+ * @brief Define static memory pool
+ * @param[out] name    name for defined memarray pool
+ * @param[in]  size    size of single memory chunk
+ * @param[in]  num     number of chunks in pool
  */
 #define MEMARRAY(name, size, num)                                         \
     static uint8_t _data_ ## name[num * size];                            \
     static memarray_t name = { .first_free = _data_ ## name };
 
+/**
+ * @brief Initialize memarray pool with free list
+ * @param[out]  mem     memarray pool to initialize
+ * @param[in]   size    size of single memory chunk
+ * @param[in]   num     number of chunks in pool
+ */
 void memarray_init(memarray_t *mem, size_t num, size_t size);
 
+/**
+ * @brief Allocate memory chunk in memarray pool
+ * @param[in,out]  mem      memarray pool to allocate block in
+ * @return pointer to newly allocated memarray chunk
+ * @return NULL on failure
+ */
 void *memarray_alloc(memarray_t *mem);
 
+/**
+ * @brief Free memory chunk in memarray pool
+ * @param[in,out]  mem      memarray pool to free block in
+ * @param[in]      ptr      pointer to memarray chunk
+ */
 void memarray_free(memarray_t *mem, void *ptr);
 
 #ifdef __cplusplus
@@ -47,3 +70,7 @@ void memarray_free(memarray_t *mem, void *ptr);
 #endif
 
 #endif /* MEMARRAY_H */
+
+/**
+ * @}
+ */
